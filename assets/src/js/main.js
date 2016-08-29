14484aa778603c94bf469ac4b73944f4c9560ba7
Mike Crantea <mihai2u@gmail.com>
2016-08-29 20:08:26 +0300

    Updated eslint rules to work better with WP standards

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-( function ( global ) {
+( function( global ) {
 	var fooConsoleLogger = require( './modules/foo-console-logger.js' );
 
 	fooConsoleLogger.helloWorld( global );
--- assets/src/js/modules/foo-console-logger.js
+++ assets/src/js/modules/foo-console-logger.js
@@ -1,8 +1,7 @@
 'use strict';
-module.exports = ( function () {
+module.exports = ( function() {
 	return {
-		helloWorld: function ( global ) {
-			global = global || window;
+		helloWorld: function( global ) {
 			global.console.info( 'Hello world! Welcome to HighStart2!\n' +
 				'Please visit https://github.com/mehigh/highstart2 for more information.' );
 		}
